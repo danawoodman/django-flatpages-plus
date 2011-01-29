67e27c81c793d3ae65afe21e93e44352c3731537
Dana Woodman <dana@danawoodman.com>
2011-01-29 01:24:09 -0800

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'danawoodman.com',
+    version = '0.1.0',
+    description = 'A more robust FlatPage app for Django.',
+    author = 'Dana Woodman',
+    author_email = 'dana@danawoodman.com',
+    url = 'https://github.com/danawoodman/django-flatpages-plus',
+    packages = find_packages(),
+)
