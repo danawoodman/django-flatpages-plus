bd35bbe2e1c8e0f97feadf4d092e13612ac7427b
Dana Woodman <dana@danawoodman.com>
2012-10-05 18:47:18 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-flatpages-plus',
-    version = '0.1',
-    description = 'A more robust FlatPage app for Django.',
-    author = 'Dana Woodman',
-    author_email = 'dana@danawoodman.com',
-    url = 'https://github.com/danawoodman/django-flatpages-plus',
-    license = 'MIT',
-    packages = find_packages(),
+    name='django-flatpages-plus',
+    version='0.1.1',
+    description='A more robust FlatPage app for Django.',
+    author='Dana Woodman',
+    author_email='dana@danawoodman.com',
+    url='https://github.com/danawoodman/django-flatpages-plus',
+    license='MIT',
+    packages=find_packages(),
 )
